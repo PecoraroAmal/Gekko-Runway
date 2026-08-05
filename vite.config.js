272f6e96476bd398b3d866a8a98bcbbba3d355c6
PecoraroAmal <amal.pecoraro@gmail.com>
2026-08-05 14:20:06 +0200

    modifica di vite.config.js per la radice

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/gekko-runway/',
   server: {
     port: 5173,
     strictPort: true
